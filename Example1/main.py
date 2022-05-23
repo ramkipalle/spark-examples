fb76a4aba4e36daf5eeb5ac116c661505776df65
ramkipalle <ramki.palle@gmail.com>
2022-05-23 18:29:58 -0400

    Initial checkin

---
+++ Example1/main.py
@@ -0,0 +1,22 @@
+
+from pyspark.sql import SparkSession
+
+
+
+if __name__ == "__main__":
+
+    # build spark session
+    spark = SparkSession.builder.appName('Example1').getOrCreate()
+
+    columns = ["name","languagesAtSchool","currentState"]
+    data = [("James,,Smith",["Java","Scala","C++"],"CA"), \
+        ("Michael,Rose,",["Spark","Java","C++"],"NJ"), \
+        ("Robert,,Williams",["CSharp","VB"],"NV")]
+
+    df = spark.createDataFrame(data=data,schema=columns)
+    df.printSchema()
+    df.show(truncate=False)
+
+    spark.stop()
+
+
