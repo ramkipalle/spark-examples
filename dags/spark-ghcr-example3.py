26f12f0743dc7ed4c273efa47ab4b620a1e4b430
ramkipalle <ramki.palle@gmail.com>
2022-05-26 21:09:37 -0400

    updated image pull secret name

--- dags/spark-ghcr-example3.py
+++ dags/spark-ghcr-example3.py
@@ -19,7 +19,7 @@ spark_pi_task = OceanSparkOperator(
     config_overrides={
         "type": "Python",
         "sparkVersion": "3.2.0",
-        "imagePullSecrets": ["github-secret"],
+        "imagePullSecrets": ["github-pull-image-secret"],
         "image": "ghcr.io/ramkipalle/spark-examples/example1:latest",
         "mainApplicationFile": "local:///opt/application/main.py"
     },
