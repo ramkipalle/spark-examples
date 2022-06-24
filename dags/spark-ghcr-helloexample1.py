61c84e7ae790e61cf2bed22566bf07495cde599d
ramkipalle <ramki.palle@gmail.com>
2022-06-24 00:02:25 -0500

    added new airflow dag

---
+++ dags/spark-ghcr-helloexample1.py
@@ -0,0 +1,31 @@
+from airflow import DAG, utils
+from ocean_spark.operators import (
+    OceanSparkOperator,
+)
+
+args = {
+    "owner": "airflow",
+    "email": [],  # ["airflow@example.com"],
+    "depends_on_past": False,
+    "start_date": utils.dates.days_ago(0, second=1),
+}
+
+
+dag = DAG(dag_id="spark-ghcr-helloexample1", default_args=args, schedule_interval=None)
+
+spark_pi_task = OceanSparkOperator(
+    task_id="spark-ghcr-helloexample1",
+    dag=dag,
+    config_overrides={
+        "type": "Python",
+        "sparkVersion": "3.2.0",
+        "imagePullSecrets": ["github-pull-image-secret"],
+        "imagePullPolicy": "Always",
+        "image": "ghcr.io/ramkipalle/spark-examples/helloexample1:latest",
+        "mainApplicationFile": "local:///opt/application/HelloExample1.py",
+        "executor": {
+            "cores": 2,
+            "instances": 1
+        }
+    }
+)
